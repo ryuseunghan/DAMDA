3ea587c5d34df91c8a880c84a163d31a95c81f5a
Seunghan Ryu <106146847+ryuseunghan@users.noreply.github.com>
2023-09-10 19:00:34 +0900

    .

--- src/main/java/com/mansa/damda/voicerecognition/VoiceRecognitionService.java
+++ src/main/java/com/mansa/damda/voicerecognition/VoiceRecognitionService.java
@@ -18,7 +18,7 @@ public class VoiceRecognitionService {
     private final StoreRepository storeRepository;
 
     public String recognizeVoice(MultipartFile voiceFile) {
-        String clientId = "39x8q030yy";             // 발급받은 Client ID
+        String clientId = "";             // 발급받은 Client ID
         String clientSecret = "";     // 발급받은 Client Secret
 
         try {
